82d75e023d9e82bba727e54d604e495453453d59
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-07-15 12:11:08 +0500

    Weather API

--- functions/weather.js
+++ functions/weather.js
@@ -1,3 +1,21 @@
 export async function onRequest(context) {
-    return new Response("Hello, world!")
+    const api_key = await context.env.SECRETS.get("API_KEY");
+    const city_name = await context.env.SECRETS.get("CITY_NAME");
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${api_key}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            return new Response("Error fetching weather data", {status: 500});
+        }
+
+        const {main, weather} = await response.json();
+        const data = {main: main[0], weather};
+
+        return new Response(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        return new Response("Error occurred while fetching weather data");
+    }
 }
