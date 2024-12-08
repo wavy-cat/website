ae76408c1350b280a972f98dfd360f7000b4e363
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-12-09 01:08:31 +0500

    Put CORS back into _middleware.ts

--- functions/weather/_middleware.ts
+++ functions/weather/_middleware.ts
@@ -1,3 +1,18 @@
+const allowOrigin = 'https://wavycat.ru, https://www.wavycat.ru';
+
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': allowOrigin,
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
+
 // Set CORS to all /weather responses
 export const onRequestGet: PagesFunction = async (context: { next: () => any; }) => {
   const response = await context.next();
