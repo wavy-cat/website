e917a37594544ab1029b6057be02f00b80ca4895
WavyCat <me@wavycat.ru>
2025-04-14 21:41:32 +0500

    Fix ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next"
 import {Geist} from "next/font/google"
 import "./globals.css"
-import {ThemeProvider} from "next-themes"
+import {ThemeProvider} from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
