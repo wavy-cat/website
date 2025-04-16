d524d7107968ac068e100ac7fec4d52c2ac50840
WavyCat <me@wavycat.ru>
2025-04-16 22:21:18 +0500

    Add doki

--- src/components/home/blocks/projects/projects.tsx
+++ src/components/home/blocks/projects/projects.tsx
@@ -21,8 +21,16 @@ export default function Projects() {
                            ]}
                            description="Plugin for JetBrains IDE to show your activity in your Discord profile."
                            buttons={[
-                             {content: "Star on Github", link: "https://github.com/wavy-cat/Cat-Activity", target: "_blank"},
-                             {content: "Marketplace", link: "https://plugins.jetbrains.com/plugin/24065-cat-activity", target: "_blank"}
+                             {
+                               content: "Star on Github",
+                               link: "https://github.com/wavy-cat/Cat-Activity",
+                               target: "_blank"
+                             },
+                             {
+                               content: "Marketplace",
+                               link: "https://plugins.jetbrains.com/plugin/24065-cat-activity",
+                               target: "_blank"
+                             }
                            ]}/>
             <SingleProject name="TotemLib"
                            badges={[
@@ -32,7 +40,11 @@ export default function Projects() {
                            ]}
                            description="Python library to generate totems of undying for Minecraft."
                            buttons={[
-                             {content: "Star on Github", link: "https://github.com/wavy-cat/wavy-totem-lib", target: "_blank"},
+                             {
+                               content: "Star on Github",
+                               link: "https://github.com/wavy-cat/wavy-totem-lib",
+                               target: "_blank"
+                             },
                              {content: "PyPI", link: "https://pypi.org/project/wavy-totem-lib", target: "_blank"},
                              {content: "Docs", link: "https://totem-lib.wavycat.ru/", target: "_blank"},
                            ]}/>
@@ -44,7 +56,11 @@ export default function Projects() {
                            ]}
                            description="A web service for generating petpet GIFs (and APNG now) based on a Discord user's avatar."
                            buttons={[
-                             {content: "Star on Github", link: "https://github.com/wavy-cat/petpet-go", target: "_blank"},
+                             {
+                               content: "Star on Github",
+                               link: "https://github.com/wavy-cat/petpet-go",
+                               target: "_blank"
+                             },
                              {content: "Try it", link: "https://pet.wavycat.ru", target: "_blank"}
                            ]}/>
             <SingleProject name="Website"
@@ -55,6 +71,20 @@ export default function Projects() {
                            description="The personalized website you are currently on. Deployed in Cloudflare Pages."
                            buttons={[
                              {content: "Star on Github", link: "https://github.com/wavy-cat/website", target: "_blank"}
+                           ]}/>
+            <SingleProject name="doki"
+                           badges={[
+                             "CLI Utility",
+                             "Go"
+                           ]}
+                           description="A minimalistic and fast port knocker."
+                           buttons={[
+                             {content: "Star on Github", link: "https://github.com/wavy-cat/doki", target: "_blank"},
+                             {
+                               content: "Install",
+                               link: "https://github.com/wavy-cat/doki/releases/latest",
+                               target: "_blank"
+                             }
                            ]}/>
           </div>
         </ScrollArea>
