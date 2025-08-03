40bd2c1f6ef6db9769eb4dee6b29571d88bec1e4
WavyCat <me@wavycat.ru>
2025-08-03 16:01:04 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wcatie.xyz',
+  site: 'https://wavycat.ru',
   vite: {
     plugins: [tailwindcss()]
   },
