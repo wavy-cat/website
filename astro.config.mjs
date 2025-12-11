b1912a5988cf4b6c6fc185cb521fb7bbfdd611e0
Catherine Caramel <root@wavycat.me>
2025-12-11 18:24:44 +0500

    Update site URL from wavycat.ru to wavycat.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wavycat.ru',
+  site: 'https://wavycat.me',
   vite: {
     plugins: [tailwindcss()]
   },
