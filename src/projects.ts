b9cfb507a79b3fe86e8d889ada330d526b0c0d10
WavyCat <root@wavycat.me>
2026-06-25 00:08:22 +0500

    refactor(page): extract socials and projects to separate modules

---
+++ src/projects.ts
@@ -0,0 +1,77 @@
+const projects = [
+  {
+    name: "website",
+    description: "the Astro website you are currently visiting.",
+    links: [
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/website",
+      },
+    ],
+  },
+  {
+    name: "cat activity",
+    description:
+      "plugin for JetBrains IDE to show your activity in your Discord profile.",
+    links: [
+      {
+        social: "marketplace",
+        url: "https://plugins.jetbrains.com/plugin/24065-cat-activity",
+      },
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/cat-activity",
+      },
+    ],
+  },
+  {
+    name: "compression station",
+    description: "implementation of RFC 9842 as a proxy server.",
+    links: [
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/compression-station",
+      },
+    ],
+  },
+  {
+    name: "totemlib",
+    description: "library to generate totems of undying for Minecraft.",
+    links: [
+      {
+        social: "docs",
+        url: "https://totemlib.wavycat.me",
+      },
+      {
+        social: "pypi",
+        url: "https://pypi.org/project/wavy-totem-lib/",
+      },
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/wavy-totem-lib",
+      },
+    ],
+  },
+  {
+    name: "doki",
+    description: "a minimalistic and fast port knocker.",
+    links: [
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/doki",
+      },
+    ],
+  },
+  {
+    name: "petpet-go",
+    description: "petpet GIF generator, written in Go.",
+    links: [
+      {
+        social: "github",
+        url: "https://github.com/wavy-cat/petpet-go",
+      },
+    ],
+  },
+];
+
+export default projects;
---
+++ src/socials.ts
@@ -0,0 +1,19 @@
+const socials = [
+  {
+    social: "telegram",
+    username: "@wavycatter",
+    url: "https://t.me/wavycatter",
+  },
+  {
+    social: "matrix",
+    username: "@wavycat:private.coffee",
+    url: "https://matrix.to/#/@wavycat:private.coffee",
+  },
+  {
+    social: "github",
+    username: "wavy-cat",
+    url: "https://github.com/wavy-cat",
+  },
+];
+
+export default socials;
